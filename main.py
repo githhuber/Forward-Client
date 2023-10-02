ade72ef9e15ee23d55a78a6f8625ca6e8852d313
githhuber <86601101+githhuber@users.noreply.github.com>
2023-10-02 22:43:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ User = Client(
     session_string=Config.STRING_SESSION
 )
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 8080))
+PORT = int(os.environ.get("PORT", 8080))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
